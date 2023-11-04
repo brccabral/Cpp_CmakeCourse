dbc9a4b98c23b9827e3cd551fa69294e9cae1ebf
Bruno Cabral <bruno.cabral@amdocs.com>
2023-11-04 16:44:26 -0700

    Git submodules

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CMake, Testing and Tooling for C/C++
 
 Cmake Udemy course https://udemy.com/course/cmake-tests-and-tooling-for-cc-projects  
+
+Add JSON as submodule
+```sh
+git submodule add --depth 1 https://github.com/nlohmann/json external/json
+```
--- app/main.cc
+++ app/main.cc
@@ -3,10 +3,17 @@
 #include "my_lib.h"
 #include "config.hpp"
 
+#include <nlohmann/json.hpp>
+
 int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
+
+    std::cout << "JSON Lib Version:"
+              << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
     print_hello_world();
 
