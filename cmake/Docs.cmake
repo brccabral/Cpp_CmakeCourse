0311311b7f21a09b398c8cc4d3b32b8d5d83e8ed
Bruno Cabral <bruno.cabral@amdocs.com>
2023-11-13 21:14:46 -0800

    use Doxygen from cmake as a new target

---
+++ cmake/Docs.cmake
@@ -0,0 +1,10 @@
+find_package(Doxygen)
+
+if (DOXYGEN_FOUND)
+message("DOXYGEN_EXECUTABLE ${DOXYGEN_EXECUTABLE}")
+add_custom_target(
+    docs
+    ${DOXYGEN_EXECUTABLE}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docs
+)
+endif()
