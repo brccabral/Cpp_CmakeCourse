1f599eeba6d4e3ed68e8bc892620cbf037de7a9c
Bruno Cabral <bruno.cabral@amdocs.com>
2023-11-13 18:49:33 -0800

    use the new dependencies

--- app/main.cc
+++ app/main.cc
@@ -4,16 +4,32 @@
 #include "config.hpp"
 
 #include <nlohmann/json.hpp>
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
 
 int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
 
-    std::cout << "JSON Lib Version:"
+    std::cout << "JSON: "
               << NLOHMANN_JSON_VERSION_MAJOR << "."
               << NLOHMANN_JSON_VERSION_MINOR << "."
               << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+    std::cout << "FMT: "
+              << FMT_VERSION << "\n";
+
+    std::cout << "CXXOPTS: "
+              << CXXOPTS__VERSION_MAJOR << "."
+              << CXXOPTS__VERSION_MINOR << "."
+              << CXXOPTS__VERSION_PATCH << "\n";
+
+    std::cout << "SPDLOG: "
+              << SPDLOG_VER_MAJOR << "."
+              << SPDLOG_VER_MINOR << "."
+              << SPDLOG_VER_PATCH << "\n";
 
     print_hello_world();
 
