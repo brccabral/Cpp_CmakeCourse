a6a62b59baf3f4ba5e2a3c402fbc4a73b2017f75
Bruno Cabral <bruno.cabral@amdocs.com>
2023-11-15 21:02:52 -0800

    ConfigSafeGuards for proper user config

---
+++ cmake/ConfigSafeGuards.cmake
@@ -0,0 +1,12 @@
+# make sure the user has created a build/output folder
+if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+    message(
+        FATAL_ERROR
+            "In-source builds not allowed. Please make a build directory.")
+endif()
+
+# if Build_Type was not set, force it to Debug
+if(NOT CMAKE_BUILD_TYPE)
+    message(STATUS "No build type selected, default to Debug")
+    set(CMAKE_BUILD_TYPE "Debug")
+endif()
