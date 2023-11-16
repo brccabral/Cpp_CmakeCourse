51e899712524ca5ae9f68089fb1be50e12d042aa
Bruno Cabral <bruno.cabral@amdocs.com>
2023-11-15 20:59:23 -0800

    custom functions with arguments

--- cmake/LTO.cmake
+++ cmake/LTO.cmake
@@ -1,5 +1,15 @@
-function(target_enable_lto TARGET ENABLE)
-    if(NOT ${ENABLE})
+function(target_enable_lto)
+    # TARGET and ENABLE are parameters that can receive one value
+    set(oneValueArgs TARGET ENABLE)
+    # after parse, all variables will be prefixed with `LTO`
+    cmake_parse_arguments(
+        LTO
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN})
+
+    if(NOT ${LTO_ENABLE})
         return()
     endif()
 
@@ -7,11 +17,11 @@ function(target_enable_lto TARGET ENABLE)
     check_ipo_supported(RESULT result OUTPUT output)
 
     if(result)
-        message(STATUS "IPO/LTO is supported!")
-        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ${ENABLE})
-        target_compile_options(${TARGET} PUBLIC "-Gy")
-        target_link_options(${TARGET} PUBLIC "-LTCG")
+    message(STATUS "IPO/LTO is supported: ${LTO_TARGET}")
+        set_property(TARGET ${LTO_TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ${LTO_ENABLE})
+        target_compile_options(${LTO_TARGET} PUBLIC "-Gy")
+        target_link_options(${LTO_TARGET} PUBLIC "-LTCG")
     else()
-        message(WARNING "IPO/LOT is not supported!.")
+        message(WARNING "IPO/LOT is not supported:  ${LTO_TARGET}")
     endif()
 endfunction()
