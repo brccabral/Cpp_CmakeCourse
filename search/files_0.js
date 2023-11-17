fd2b79f2f016c7adbf7e4c36cfb7c901df4f1ead
brccabral <>
2023-11-17 20:56:48 +0000

    brccabral published a site update

--- search/files_0.js
+++ search/files_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['main_2ecc_5',['main.cc',['../main_8cc.html',1,'']]],
-  ['my_5flib_2ecc_6',['my_lib.cc',['../my__lib_8cc.html',1,'']]],
-  ['my_5flib_2eh_7',['my_lib.h',['../my__lib_8h.html',1,'']]]
+  ['main_2ecc_4',['main.cc',['../main_8cc.html',1,'']]],
+  ['my_5flib_2ecc_5',['my_lib.cc',['../my__lib_8cc.html',1,'']]]
 ];
